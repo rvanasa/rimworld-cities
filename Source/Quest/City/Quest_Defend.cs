7b89b34759887caa6fe85c82539fd30d769ec684
Ryan Vandersmith <ryanvandersmith@gmail.com>
2020-03-02 00:28:48 -0700

    Fix city defense quest item claiming

--- Source/Quest/City/Quest_Defend.cs
+++ Source/Quest/City/Quest_Defend.cs
@@ -53,7 +53,7 @@ namespace Cities {
 
         public override void OnMapGenerated(Map map) {
             if (map.Parent == city) {
-                map.GetComponent<MapComponent_City>().cityOwnedThings.Clear();
+                // map.GetComponent<MapComponent_City>().cityOwnedThings.Clear();
                 // var playerFaction = Faction.OfPlayer;
                 // foreach (var thing in map.listerThings.AllThings) {
                 //     if (!(thing is Pawn) && thing.def.CanHaveFaction) {
