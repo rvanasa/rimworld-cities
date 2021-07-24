7bea5154a5d5996968c54b7dc13b31cda1c49ca5
Ryan Vandersmith <ryanvandersmith@gmail.com>
2021-07-24 13:04:48 -0600

    Add debug action: `Populate Ghost Cities`

---
+++ Source/DebugActionsCities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace Cities {
+    public static class DebugActionsCities {
+        
+        [DebugAction("General", actionType = DebugActionType.ToolWorld, allowedGameStates = AllowedGameStates.PlayingOnWorld)]
+        public static void PopulateGhostCities() {
+            foreach (var settlement in Find.WorldObjects.Settlements) {
+                if (settlement is City city && !city.Destroyed && city.inhabitantFaction == null) {
+                    city.inhabitantFaction = city.Faction;
+                }
+            }
+        }
+    }
+}
