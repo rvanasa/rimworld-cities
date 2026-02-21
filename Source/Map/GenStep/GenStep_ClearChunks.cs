2e6b5d4599d451522940e13c4f5ed0111d1df872
A Certain Existence <a.certain.existence@gmail.com>
2026-02-22 03:27:39 +0800

    Clear chunks on floored terrains for faction cities and citadels (#49)

---
+++ Source/Map/GenStep/GenStep_ClearChunks.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using Verse;
+
+namespace Cities {
+    public class GenStep_ClearChunks : GenStep {
+        public override int SeedPart => GetType().Name.GetHashCode();
+
+        public override void Generate(Map map, GenStepParams parms) {
+            foreach (Thing thing in map.spawnedThings) {
+                if (thing.HasThingCategory(ThingCategoryDefOf.StoneChunks)) {
+                    IntVec3 position = thing.Position;
+
+                    if (!map.terrainGrid.TerrainAt(position).natural) {
+                        thing.Destroy();
+                    }
+                }
+            }
+
+            List<Thing> thingsToDestroy = map.spawnedThings.Where(thing => !map.terrainGrid.TerrainAt(thing.Position).natural
+                && (thing.HasThingCategory(ThingCategoryDefOf.StoneChunks) || thing is Filth)
+                && thing.def.destroyable
+                && !thing.Destroyed).ToList();
+
+            foreach (Thing thing in thingsToDestroy) {
+                thing.Destroy();
+            }
+        }
+    }
+}
