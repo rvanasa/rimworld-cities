a871a55de52f018dd676489b0fa9f8de659fb1a4
A Certain Existence <a.certain.existence@gmail.com>
2026-02-21 23:19:13 +0800

    Avoid spawning stumps in fields

--- Source/Map/GenStep/GenStep_Fields.cs
+++ Source/Map/GenStep/GenStep_Fields.cs
@@ -2,25 +2,30 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using RimWorld;
-using RimWorld.Planet;
-using RimWorld.BaseGen;
 using Verse;
-using Verse.AI.Group;
 
 namespace Cities {
-
     public class GenStep_Fields : GenStep_RectScatterer {
         public float density = 0.9F;
         public AltitudeLayer altitudeLayer = AltitudeLayer.LowPlant;
         public List<string> excludePlants = new List<string>();
 
         public override void GenerateRect(Stencil s) {
-            var plant = DefDatabase<ThingDef>.AllDefs
-                .Where(t => t.category == ThingCategory.Plant && t.altitudeLayer == altitudeLayer && !t.plant.cavePlant && !excludePlants.Contains(t.defName))
+            var plantDef = DefDatabase<ThingDef>.AllDefs
+                .Where(t => t.category == ThingCategory.Plant && t.altitudeLayer == altitudeLayer && !t.plant.cavePlant && !t.plant.isStump && !excludePlants.Contains(t.defName))
                 .RandomElement();
+
+            // Growth and age follow the rules from `RimWorld.BaseGen.SymbolResolver_CultivatedPlants.Resolve`.
+            float growth = Rand.Range(0.2f, 1f);
+            int age = plantDef.plant.LimitedLifespan ? Rand.Range(0, Mathf.Max(plantDef.plant.LifespanTicks - 2500, 0)) : -1;
+
             foreach (var pos in s.bounds.Cells) {
-                if (s.Chance(density) && pos.GetFirstThing<Thing>(s.map) == null) {
-                    GenSpawn.Spawn(plant, pos, s.map);
+                if (s.Chance(density) && pos.GetFirstThing<Thing>(s.map) == null && GenSpawn.Spawn(plantDef, pos, s.map) is Plant plant) {
+                    plant.Growth = growth;
+
+                    if (age >= 0) {
+                        plant.Age = age;
+                    }
                 }
             }
         }
